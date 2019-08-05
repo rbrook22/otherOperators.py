e82eae246f1affb579dc8c327f32c9473b9f1f40
Richard Brooks <rickbrooks22@gmail.com>
2019-08-05 10:00:25 -0400

    added second range operator

--- otherOperators.py
+++ otherOperators.py
@@ -1,7 +1,10 @@
 #File using other built in functions/operators
-
+print('I will be printing the numbers from range 1-11')
 for num in range(11):
     print(num)
 
 #Printing using range and start position
+print("I will be printing the numbers from range 1-11 starting at 4")
+for num in range(4,11):
+    print(num)
 
